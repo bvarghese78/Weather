f87348bd842b59abbcf8bb22edb754151ca0a025
Benji Varghese <benjivarghese@gmail.com>
2015-09-09 22:49:49 -0500

    Style added to exercise drop down menu section

--- MvcApplication/App_Start/BundleConfig.cs
+++ MvcApplication/App_Start/BundleConfig.cs
@@ -39,7 +39,7 @@ namespace MvcApplication
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css",
-                "~/Content/sits.css"));
+                "~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/highchart").Include("~/scripts/highcharts-4.0.1/js/highcharts.js"));
         }
